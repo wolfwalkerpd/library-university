69e9b27da79861144bb715b5e873b1510cf3b6bf
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-06-25 22:50:09 +0330

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+const Home = () => (
+    <>
+      <BookOverview />
+      
+      <BookList />
+    </>
+  );
+
+
+export default Home;
